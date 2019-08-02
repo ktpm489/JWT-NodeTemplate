5bd6acb4016597051653ea68f05109209c1a630b
ktpm489 <hoangvu@bacoor.co>
2019-08-02 13:59:14 +0700

    Change catch exception

--- middleware.js
+++ middleware.js
@@ -3,22 +3,27 @@ const config = require('./config.js');
 
 let checkToken = (req, res, next) => {
   let token = req.headers['x-access-token'] || req.headers['authorization']; // express headers are auto converted to lowercase
-  if (token.startsWith('Bearer ')) {
+  if (token && token.startsWith('Bearer ')) {
     // Remove Bearer from string
     token = token.slice(7, token.length);
-  }
-  if (token) {
-    jwt.verify(token, config.secret, (err, decoded) => {
-      if (err) {
-        return res.json({
-          success: false,
-          message: 'Token is not valid'
-        });
-      } else {
-        req.decoded = decoded;
-        next();
-      }
-    });
+    if (token) {
+      jwt.verify(token, config.secret, (err, decoded) => {
+        if (err) {
+          return res.json({
+            success: false,
+            message: 'Token is not valid'
+          });
+        } else {
+          req.decoded = decoded;
+          next();
+        }
+      });
+    } else {
+      return res.json({
+        success: false,
+        message: 'Auth token is not supplied'
+      });
+    }
   } else {
     return res.json({
       success: false,
